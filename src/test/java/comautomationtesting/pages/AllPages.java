396b5a539759b862eaa2e2df55d5be2294a4dc63
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-01 21:57:19 +0300

    allpages add project

--- src/test/java/comautomationtesting/pages/AllPages.java
+++ src/test/java/comautomationtesting/pages/AllPages.java
@@ -1,4 +1,24 @@
 package comautomationtesting.pages;
 
 public class AllPages {
+    public AllPages() {
+    }
+    private BasketPage basketPage;
+    private CheckoutPage checkoutPage;
+
+    public CheckoutPage checkoutPage() {
+        if (checkoutPage == null) {
+            checkoutPage = new CheckoutPage();
+        }
+        return checkoutPage;
+    }
+
+    public BasketPage basketPage() {
+        if (basketPage == null) {
+            basketPage = new BasketPage();
+        }
+        return basketPage;
+    }
+
+
 }
