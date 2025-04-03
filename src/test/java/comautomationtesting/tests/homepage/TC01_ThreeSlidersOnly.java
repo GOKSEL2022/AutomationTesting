1963a31bc6d8fe0d53edd12886c0657e6f5943bf
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-04 00:12:37 +0300

    04.04.2025

--- src/test/java/comautomationtesting/tests/homepage/TC01_ThreeSlidersOnly.java
+++ src/test/java/comautomationtesting/tests/homepage/TC01_ThreeSlidersOnly.java
@@ -1,5 +1,5 @@
 package comautomationtesting.tests.homepage;
-import comautomationtesting.pages.HomePage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
 import org.testng.Reporter;
@@ -11,25 +11,24 @@ import static org.testng.AssertJUnit.assertEquals;
 public final class TC01_ThreeSlidersOnly {
     @Test
     public void ThreeSlidersOnlyTest() {
-
-        HomePage homepage = new HomePage();
+        AllPages allPages=new AllPages();
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on Shop Menu
-        clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         //4) Now click on Home menu button
         Driver.getDriver().navigate().refresh();
-        waitAndClick(homepage.shopButton);
+        waitAndClick(allPages.homePage().shopButton);
         Driver.getDriver().navigate().back();
 
         //5) Test whether the Home page has Three Sliders only
-        assertEquals(3,homepage.threeSlidersHome.size());
+        assertEquals(3,allPages.homePage().threeSlidersHome.size());
         //ReusableMethods.scrollIntoViewJS(homepage.threeSliders);
         //assert homepage.threeSliders.isDisplayed();
 
         //6) The Home page must contains only three sliders
-        assertEquals(homepage.arriwals.size(), 3);
+        assertEquals(allPages.homePage().arriwals.size(), 3);
         Reporter.log("Sayfada sadece 3 urun oldugu dogrulanir");
 
         //Driver.closeDriver();
--- src/test/java/comautomationtesting/tests/homepage/TC02_ThreeArriwalsOnly.java
+++ src/test/java/comautomationtesting/tests/homepage/TC02_ThreeArriwalsOnly.java
@@ -1,34 +1,34 @@
 package comautomationtesting.tests.homepage;
-
-import comautomationtesting.pages.HomePage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
+
 public class TC02_ThreeArriwalsOnly {
     @Test
     public void ThreeArriwalsOnly() {
-        HomePage homepage=new HomePage();
+        AllPages allPages=new AllPages();
 
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         Reporter.log("Sayfaya gidilir");
         //3) Click on Shop Menu
-        ReusableMethods.clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         Reporter.log("Shop butona tiklanir" );
 
         //4) Now click on Home menu button
         Driver.getDriver().navigate().refresh();
-        ReusableMethods.clickWithJS(homepage.shopButton);
-        ReusableMethods.clickWithJS(homepage.homeButton);
+        clickWithJS(allPages.homePage().shopButton);
+        clickWithJS(allPages.homePage().homeButton);
         Reporter.log("Home butona tiklanir");
         //5) Test whether the Home page has Three Arrivals only
         //6) The Home page must contains only three Arrivals
-        Assert.assertEquals(homepage.arriwals.size(), 3);
+        Assert.assertEquals(allPages.homePage().arriwals.size(), 3);
         Reporter.log("Sayfada sadece 3 urun oldugu dogrulanir");
         Driver.closeDriver();
 
