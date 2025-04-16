12ec79b197e4d6a67645c9b6bd0c6e78fc6e7d77
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-17 00:21:05 +0300

    17.04.2025 TC19 and TC20 update

--- src/test/java/comautomationtesting/tests/TC20_Login02_İncorretUsernameAndİncorretPassword.java
+++ src/test/java/comautomationtesting/tests/TC20_Login02_İncorretUsernameAndİncorretPassword.java
@@ -1,17 +1,12 @@
 package comautomationtesting.tests;
-
 import com.github.javafaker.Faker;
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.MyAccountPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
 public class TC20_Login02_İncorretUsernameAndİncorretPassword {
-    HomePage homepage = new HomePage();
-    MyAccountPage myAccountPage = new MyAccountPage();
+    AllPages allPages=new AllPages();
     Faker faker = new Faker();
     @Test
     public void İncorretUsernameAndİncorretPassword() {
@@ -21,18 +16,17 @@ public class TC20_Login02_İncorretUsernameAndİncorretPassword {
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on My Account Menu
-        ReusableMethods.clickWithJS(homepage.myAccountButtonHome);
+        clickWithJS(allPages.homePage().myAccountButtonHome);
         Driver.getDriver().navigate().refresh();
-        homepage.myAccountButtonHome.click();
+        allPages.homePage().myAccountButtonHome.click();
         //4) Enter incorrect username in username textbox
         //5) Enter incorrect password in password textbox.
-        myAccountPage.textBoxUsernameLoginMyAccount.sendKeys(email);
-        myAccountPage.textBoxPasswordLoginMyAccount.sendKeys(password);
+        allPages.myAccountPage().textBoxUsernameLoginMyAccount.sendKeys(email);
+        allPages.myAccountPage().textBoxPasswordLoginMyAccount.sendKeys(password);
         //6) Click on login button
-        myAccountPage.loginButtonLoginMyAccount.click();
+        allPages.myAccountPage().loginButtonLoginMyAccount.click();
         //7) Proper error must be displayed(ie Invalid username) and prompt to enter login again
-        Assert.assertTrue(myAccountPage.textErrorMessageMyAccount.getText().contains("A user could not be found with this email address."));
+        assert allPages.myAccountPage().textErrorMessageMyAccount.getText().contains("A user could not be found with this email address.");
         Driver.closeDriver();
-
     }
 }
