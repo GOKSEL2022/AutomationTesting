1b5557b20e51bdb77ed585f4d7d72d0eeb82e914
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-19 14:09:56 +0300

    19.04.2025

--- src/test/java/comautomationtesting/tests/TC24_Login06_PasswordShouldBeMasked.java
+++ src/test/java/comautomationtesting/tests/TC24_Login06_PasswordShouldBeMasked.java
@@ -1,37 +1,30 @@
 package comautomationtesting.tests;
-
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.MyAccountPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
+import static comautomationtesting.utilities.ReusableMethods.getScreenshot;
 public class TC24_Login06_PasswordShouldBeMasked {
-    HomePage homepage=new HomePage();
-    MyAccountPage myAccountPage=new MyAccountPage();
-
+    AllPages allPages=new AllPages();
     @Test
     public void PasswordShouldBeMasked() throws IOException {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on My Account Menu
-        ReusableMethods.clickWithJS(homepage.myAccountButtonHome);
+        clickWithJS(allPages.homePage().myAccountButtonHome);
         Driver.getDriver().navigate().refresh();
-        homepage.myAccountButtonHome.click();
+        allPages.homePage().myAccountButtonHome.click();
         //4) Enter the password field with some characters.
-        myAccountPage.textBoxPasswordLoginMyAccount.sendKeys("***+++???");
+        allPages.myAccountPage().textBoxPasswordLoginMyAccount.sendKeys("***+++???");
         //5) The password field should display the characters in asterisks or bullets such that the password is not visible on the screen
         //Assert.assertTrue(myAccountPage.textBoxPasswordLoginMyAccount.getText().contains("........."));
         //Assert.assertTrue(myAccountPage.textBoxPasswordLoginMyAccount.getText().contains("***+++???"));
         //Assert.assertTrue(myAccountPage.textBoxPasswordLoginMyAccount.getAttribute("***+++???").contains("........."));
-        Assert.assertTrue(myAccountPage.textBoxPasswordLoginMyAccount.isDisplayed());
-        ReusableMethods.getScreenshot("Parola degeri gizli");
+        assert allPages.myAccountPage().textBoxPasswordLoginMyAccount.isDisplayed();
+        getScreenshot("Parola degeri gizli");
         Driver.closeDriver();
-
     }
 }
