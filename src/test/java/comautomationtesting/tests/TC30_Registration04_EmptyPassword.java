88451dd493532ac2f688a9b67312de151a1b3114
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-22 02:29:13 +0300

    22.04.2025

--- src/test/java/comautomationtesting/tests/TC30_Registration04_EmptyPassword.java
+++ src/test/java/comautomationtesting/tests/TC30_Registration04_EmptyPassword.java
@@ -1,36 +1,29 @@
 package comautomationtesting.tests;
-
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.MyAccountPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
 import org.openqa.selenium.Keys;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
 public class TC30_Registration04_EmptyPassword {
-    HomePage homepage=new HomePage();
-    MyAccountPage myAccountPage=new MyAccountPage();
+    AllPages allPages=new AllPages();
     @Test
     public void EmptyPassword() {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on My Account Menu
-        ReusableMethods.clickWithJS(homepage.myAccountButtonHome);
+        clickWithJS(allPages.homePage().myAccountButtonHome);
         Driver.getDriver().navigate().refresh();
-        homepage.myAccountButtonHome.click();
+        allPages.homePage().myAccountButtonHome.click();
         //4) Enter valid Email Address in Email-Address textbox
-        myAccountPage.textBoxEmailRegisterMyAccount.sendKeys(ConfigurationReader.getProperty("email"));
+        allPages.myAccountPage().textBoxEmailRegisterMyAccount.sendKeys(ConfigurationReader.getProperty("email"));
         //5) Enter empty password in password textbox
-        myAccountPage.textBoxPasswordRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
+        allPages.myAccountPage().textBoxPasswordRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
         //6) Click on Register button
-        myAccountPage.registerButtonMyAccount.click();
+        allPages.myAccountPage().registerButtonMyAccount.click();
         //7) Registration must fail with a warning message(ie please enter an account password)
-        Assert.assertTrue(myAccountPage.registerButtonMyAccount.isDisplayed());
+        assert allPages.myAccountPage().registerButtonMyAccount.isDisplayed();
         Driver.closeDriver();
-
-
     }
 }
