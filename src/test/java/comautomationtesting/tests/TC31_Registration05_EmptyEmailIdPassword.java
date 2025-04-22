5245c370cb336d32e7c73419ff547f1ffb1dca09
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-23 00:28:05 +0300

    23.04.2025 TC31 update

--- src/test/java/comautomationtesting/tests/TC31_Registration05_EmptyEmailIdPassword.java
+++ src/test/java/comautomationtesting/tests/TC31_Registration05_EmptyEmailIdPassword.java
@@ -1,34 +1,28 @@
 package comautomationtesting.tests;
-
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.MyAccountPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
 import org.openqa.selenium.Keys;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
 public class TC31_Registration05_EmptyEmailIdPassword {
-    HomePage homepage=new HomePage();
-    MyAccountPage myAccountPage=new MyAccountPage();
+    AllPages allPages=new AllPages();
     @Test
     public void EmptyEmailIdPassword() {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on My Account Menu
-        ReusableMethods.clickWithJS(homepage.myAccountButtonHome);
+        clickWithJS(allPages.homePage().myAccountButtonHome);
         Driver.getDriver().navigate().refresh();
-        homepage.myAccountButtonHome.click();
+        allPages.homePage().myAccountButtonHome.click();
         //4) Enter empty Email Address in Email-Address textbox
-        myAccountPage.textBoxEmailRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
-        myAccountPage.textBoxPasswordRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
+        allPages.myAccountPage().textBoxEmailRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
+        allPages.myAccountPage().textBoxPasswordRegisterMyAccount.sendKeys(Keys.SPACE,Keys.SPACE,Keys.SPACE);
         //6) Click on Register button
-        myAccountPage.registerButtonMyAccount.click();
+        allPages.myAccountPage().registerButtonMyAccount.click();
         //7) Registration must fail with a warning message(ie please provide valid email address)
-        Assert.assertTrue(myAccountPage.registerButtonMyAccount.isDisplayed());
+        assert allPages.myAccountPage().registerButtonMyAccount.isDisplayed();
         Driver.closeDriver();
     }
-
 }
