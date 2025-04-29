3ce841fb3ae44e003f986a628bc853513fd34f39
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-30 00:28:20 +0300

    30.04.2025 TC39 update

--- src/test/java/comautomationtesting/tests/TC39_Shop08_ReadMoreFunctionality.java
+++ src/test/java/comautomationtesting/tests/TC39_Shop08_ReadMoreFunctionality.java
@@ -1,35 +1,28 @@
 package comautomationtesting.tests;
-
-import comautomationtesting.pages.BasketPage;
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.ShopPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
 public class TC39_Shop08_ReadMoreFunctionality {
-    HomePage homepage = new HomePage();
-    ShopPage shopPage=new ShopPage();
-    BasketPage basketPage=new BasketPage();
+    AllPages allPages=new AllPages();
     @Test
     public void ReadMoreFunctionality() {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on Shop Menu
-        ReusableMethods.clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         Driver.getDriver().navigate().refresh();
-        ReusableMethods.clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         //4) Click on read more button in home page
-        shopPage.addToBasketButtonAndroidQickStartShop.click();
+        allPages.shopPage().addToBasketButtonAndroidQickStartShop.click();
         //5) Read More option indicates the Out Of Stock.
-        shopPage.vievBasketButtonAndroidQickStartShop.click();
+        allPages.shopPage().vievBasketButtonAndroidQickStartShop.click();
         //6) User cannot add the product which has read more option as it was out of stock.
-        Assert.assertTrue(shopPage.androidQickStartQideShop.isDisplayed());
-        basketPage.removeProductButtonBasket.click();
-        Assert.assertTrue(basketPage.textEmptyBasket.isDisplayed());
+        assert allPages.shopPage().androidQickStartQideShop.isDisplayed();
+        allPages.basketPage().removeProductButtonBasket.click();
+        assert allPages.basketPage().textEmptyBasket.isDisplayed();
         Driver.closeDriver();
     }
 }
