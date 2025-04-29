6c882f7c598c02e7e83e4626b1d40445078c6cd5
Göksel Celik <gokselcelik5255@gmail.com>
2025-04-29 13:46:21 +0300

    29.04.2025 TC38 update

--- src/test/java/comautomationtesting/tests/TC38_Shop07_DefaultSortingHighToLowItemFunctionality.java
+++ src/test/java/comautomationtesting/tests/TC38_Shop07_DefaultSortingHighToLowItemFunctionality.java
@@ -1,33 +1,26 @@
 package comautomationtesting.tests;
-
-import comautomationtesting.pages.HomePage;
-import comautomationtesting.pages.ShopPage;
+import comautomationtesting.pages.AllPages;
 import comautomationtesting.utilities.ConfigurationReader;
 import comautomationtesting.utilities.Driver;
-import comautomationtesting.utilities.ReusableMethods;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static comautomationtesting.utilities.ReusableMethods.clickWithJS;
 public class TC38_Shop07_DefaultSortingHighToLowItemFunctionality {
-    HomePage homepage = new HomePage();
-    ShopPage shopPage=new ShopPage();
+    AllPages allPages=new AllPages();
     @Test
     public void DefaultSortingHighToLowItemFunctionality() {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
         //3) Click on Shop Menu
-        ReusableMethods.clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         Driver.getDriver().navigate().refresh();
-        ReusableMethods.clickWithJS(homepage.shopButton);
+        clickWithJS(allPages.homePage().shopButton);
         //4) Click on Sort by High to Low Item in Default sorting dropdown
-        shopPage.defaultSortingDropDownShop.click();
+        allPages.shopPage().defaultSortingDropDownShop.click();
         //5) Now user can view the popular products only
-        Select select =new Select(shopPage.defaultSortingDropDownShop);
-        select.selectByVisibleText("Sort by price: high to low");
-        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("orderby=price-desc"));
-
+        new Select(allPages.shopPage().defaultSortingDropDownShop).selectByVisibleText("Sort by price: high to low");
+        assert Driver.getDriver().getCurrentUrl().contains("orderby=price-desc");
         Driver.closeDriver();
     }
 }
